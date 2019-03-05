7f0ccc739dacf1c65e8a17b5f9eae51c2847402c
jiwon8578 <jiwon_h98@naver.com>
2019-03-05 19:50:33 +0900

    View - Oscilloscope Waveform Window 구현!

--- Oscilloscope/mainwindow.cpp
+++ Oscilloscope/mainwindow.cpp
@@ -1,6 +1,5 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "aboutdialog.h"
 #include <QVBoxLayout>
 #include <QMdiSubWindow>
 #include "fftplot.h"
@@ -21,8 +20,12 @@ MainWindow::MainWindow(QWidget *parent) :
     //createMenus();
     //createActions();
 
+    osSetupWin = ui->mdiArea->addSubWindow(fftSetupObj);
+    osSetupWin->resize(400, 400);
+    osSetupWin->show();
+
     loadSubWindow_plot(new freqPlot(this));
-    loadSubWindow_plot(new fftPlot(this));
+    //loadSubWindow_plot(new fftPlot(this));
 
     loadSubWindow_setup(new OsciSetup(this));
     loadSubWindow_setup(new fftsetup(this));
@@ -36,155 +39,12 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-/*
-void MainWindow::createMenus()
-{
-    menuBar()->setStyleSheet("background-color: #454548"); //menubar 색상 변경
-    menuBar()->setStyleSheet("color: #FFFFFF"); //menubar 글씨 색상 변경
-    //menuBar()->setFont("");
-
-    //FILE만들기.
-    file = menuBar() -> addMenu("File");
-    load_setups = file -> addAction("Load Setups");
-    save_setups = file -> addAction("Save Setups");
-    file -> addSeparator();
-    open_file = file -> addAction("Open File");
-    save_file = file -> addAction("Save File");
-    file -> addSeparator();
-    print = file -> addAction("Print");
-    //exit = file -> addAction("Exit"); // line#44로 대체.
-
-    QAction *quit = new QAction("Exit", this);
-    file -> addAction(quit);
-    connect(quit, &QAction::triggered, qApp, QApplication::quit);
-
-    //VIEW만들기.
-    view = menuBar() -> addMenu("View");
-    osWaveFoam = view -> addAction("Oscilloscope Waveform Window");
-    osSetupWindow = view -> addAction("Oscilloscope Setup Window");
-    view -> addSeparator();
-    FFTWaveWindow = view -> addAction("FFT Wavefoam Window");
-    FFTSetWindow = view -> addAction("FFT Setup Window");
-    view -> addSeparator();
-    toolBar = view -> addAction("Tool Bar");
-    arrangeAllWindow = view -> addAction("Arrange All Window");
-    fullScreen = view -> addAction("Full Screen");
-
-    //setup만들기.
-    setup = menuBar() -> addMenu("Setup");
-    osSetup = setup -> addAction("Oscilloscope Setup");
-    fftSetup = setup -> addAction("FFT Analyzer Setup");
-    resetDefault = setup -> addAction("All Setup Reset");
-
-    //utility만들기.
-    utility = menuBar() -> addMenu("Utility");
-    transSetup = utility -> addAction("Transducer Setup");
-    testTargetSetup = utility -> addAction("Test Target Setup");
-    screenCapture = utility -> addAction("Screen Capture");
-    deleteSetup = utility -> addAction("Delete Setup");
-
-    //Cursor Menu
-    cursorMenu = menuBar()->addMenu("Cursor");
-    noneAct = cursorMenu->addAction("None");
-    crossAct = cursorMenu->addAction("Cross");
-    verticalAct = cursorMenu->addAction("Vertical");
-    horizontalAct = cursorMenu->addAction("Horizontal");
-
-    //Acquire Menu
-    acquireMenu = menuBar()->addMenu("Acquire");
-    startAct = acquireMenu->addAction("Start");
-    stopAct = acquireMenu->addAction("Stop");
-    acquireMenu->addSeparator();
-    zoomInAct = acquireMenu->addAction("Zoom in");
-    zoomOutAct = acquireMenu->addAction("Zoom out");
-    acquireMenu->addSeparator();
-    acquisitionAct = acquireMenu->addAction("Acquisition");
-    interpolationAct = acquireMenu->addAction("Interpolation");
-    autoSetupAct = acquireMenu->addAction("Auto setup");
-
-    //Measure Menu
-    measureMenu = menuBar()->addMenu("Measure");
-
-    //Measure Menu - Oscilloscope Vertical Submenu
-    osciVerticalMenu = measureMenu->addMenu("Oscilloscope Vertical");
-    maximumAct = osciVerticalMenu->addAction("Maximum");
-    minimunAct = osciVerticalMenu->addAction("Minimum");
-    peakToPeakAct = osciVerticalMenu->addAction("Peak to Peak");
-    topAct = osciVerticalMenu->addAction("Top");
-    baseAct = osciVerticalMenu->addAction("Base");
-    rmsAct = osciVerticalMenu->addAction("RMS");
-    amplitudeAct = osciVerticalMenu->addAction("Amplitude");
-    meanAct = osciVerticalMenu->addAction("Mean");
-    cycleMeanAct = osciVerticalMenu->addAction("Cycle Mean");
-    preshootAct = osciVerticalMenu->addAction("Preshoot");
-
-    //Measure Menu - Oscilloscope Horizontal Submenu
-    osciHorizontalMenu = measureMenu->addMenu("Oscilloscope Horizontal");
-    periodAct = osciHorizontalMenu->addAction("Period");
-    frequencyAct = osciHorizontalMenu->addAction("Frequency");
-    riseTimeAct = osciHorizontalMenu->addAction("Rise Time");
-    fallTimeAct = osciHorizontalMenu->addAction("Fall Time");
-    pDutyCycleAct = osciHorizontalMenu->addAction("+Duty Cycle");
-    nDutyCycleAct = osciHorizontalMenu->addAction("-Duty Cycle");
-    pPulseWidthAct = osciHorizontalMenu->addAction("+Pulse Width");
-    nPulseWidthAct = osciHorizontalMenu->addAction("-Pulse Width");
-
-    measureMenu->addSeparator();
-
-    //Measure Menu - FFT Measurement Submenu
-    fftMeasurementMenu = measureMenu->addMenu("FFT Measurement");
-    snrAct = fftMeasurementMenu->addAction("SNR");
-    thdAct = fftMeasurementMenu->addAction("THD");
-    enobAct = fftMeasurementMenu->addAction("ENOB");
-    sfdrAct = fftMeasurementMenu->addAction("SFDR");
-    sinadAct = fftMeasurementMenu->addAction("SINAD");
-    totalPowerAct = fftMeasurementMenu->addAction("TotalPower");
-    peakFrequencyAct = fftMeasurementMenu->addAction("Peak Frequency");
-
-    measureMenu->addSeparator();
-    clearDisplayAct = measureMenu->addAction("Clear display");
-
-    //Help Menu
-    helpMenu = menuBar()->addMenu("Help");
-    contentsAct = helpMenu->addAction("Contents");
-    aboutAct = helpMenu->addAction("About..");
-}
-*/
-
-void MainWindow::about()
-{
-    AboutDialog aboutDialog;
-    aboutDialog.exec();
-}
-
 void MainWindow::osciSetupFunc()
 {
     if(isOsciSetupOpen == false) {
         loadSubWindow_setup(new OsciSetup(this));
     }
 }
-
-void MainWindow::transducerSetup()
-{
-    TransducerSetupDialog transducersetupDialog;
-    transducersetupDialog.exec();
-}
-
-void MainWindow::ttSetup()
-{
-    TestTargetSetupDialog ttSetupDialog;
-    ttSetupDialog.exec();
-}
-
-void MainWindow::createActions()
-{
-    connect(aboutAct, &QAction::triggered, this, &MainWindow::about); // (변수명, 액션, parent, about함수를 실행시켜라)
-    connect(osSetup, &QAction::triggered, this, &MainWindow::osciSetupFunc);
-    connect(transSetup, &QAction::triggered, this, &MainWindow::transducerSetup);
-    connect(testTargetSetup, &QAction::triggered, this, &MainWindow::ttSetup);
-
-}
-
 
 void MainWindow::loadSubWindow_plot(QWidget *widget) //mdiArea에 sub window를 띄우는 함수 - plot용
 {
@@ -203,3 +63,13 @@ void MainWindow::loadSubWindow_setup(QWidget *widget) //mdiArea에 sub window를
 
 
 
+
+void MainWindow::on_actionOscilloscope_triggered()
+{
+    if(osSetupWinCount % 2) {
+            osSetupWin->show();
+        } else {
+            osSetupWin->hide();
+        }
+        osSetupWinCount++;
+}
--- Oscilloscope/mainwindow.h
+++ Oscilloscope/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMdiSubWindow>
+#include "fftsetup.h"
 
 namespace Ui {
 class MainWindow;
@@ -17,112 +19,21 @@ public:
     static bool isOsciSetupOpen;
 
 private slots:
-    void about();
     void osciSetupFunc();
-    void transducerSetup();
-    void ttSetup();
 
+    void on_actionOscilloscope_triggered();
 
 private:
     Ui::MainWindow *ui;
-    //File을 만들어볼게요.
-    QMenu *file;
-    QAction *load_setups;
-    QAction *save_setups;
-    QAction *open_file;
-    QAction *save_file;
-    QAction *print;
-    QAction *exit;
 
-    //VIEW를 만들어볼게요.
-    QMenu *view;
-    QAction *osWaveFoam;
-    QAction *osSetupWindow;
-    QAction *FFTWaveWindow;
-    QAction *FFTSetWindow;
-    QAction *toolBar;
-    QAction *arrangeAllWindow;
-    QAction *fullScreen;
-
-    //Setup을 만들어볼게요.
-    QMenu *setup;
-    QAction *osSetup;
-    QAction *fftSetup;
-    QAction *resetDefault;
-
-    //Utility를 만들어볼게요.
-    QMenu *utility;
-    QAction *transSetup;
-    QAction *testTargetSetup;
-    QAction *screenCapture;
-    QAction *deleteSetup;
-
-    //Cursor Menu
-    QMenu *cursorMenu;
-    QAction *noneAct;
-    QAction *crossAct;
-    QAction *verticalAct;
-    QAction *horizontalAct;
-
-    //Acquire Menu
-    QMenu *acquireMenu;
-    QAction *startAct;
-    QAction *stopAct;
-    QAction *zoomInAct;
-    QAction *zoomOutAct;
-    QAction *acquisitionAct;
-    QAction *interpolationAct;
-    QAction *autoSetupAct;
-
-    //Measure Menu
-    QMenu *measureMenu;
-    QMenu *osciVerticalMenu; //submenu
-    QAction *maximumAct;
-    QAction *minimunAct;
-    QAction *peakToPeakAct;
-    QAction *topAct;
-    QAction *baseAct;
-    QAction *rmsAct;
-    QAction *amplitudeAct;
-    QAction *meanAct;
-    QAction *cycleMeanAct;
-    QAction *preshootAct;
-    QMenu *osciHorizontalMenu; //submenu
-    QAction *periodAct;
-    QAction *frequencyAct;
-    QAction *riseTimeAct;
-    QAction *fallTimeAct;
-    QAction *pDutyCycleAct;
-    QAction *nDutyCycleAct;
-    QAction *pPulseWidthAct;
-    QAction *nPulseWidthAct;
-    QMenu *fftMeasurementMenu; //submenu
-    QAction *snrAct;
-    QAction *thdAct;
-    QAction *enobAct;
-    QAction *sfdrAct;
-    QAction *sinadAct;
-    QAction *totalPowerAct;
-    QAction *peakFrequencyAct;
-    QAction *clearDisplayAct;
-
-    //Help Menu
-    QMenu *helpMenu;
-    QAction *contentsAct;
-    QAction *aboutAct;
-
-    //Make plot
-    QWidget *customPlot;
-
-    void menubars();
-    void createMenus();
-    void createActions();
     void loadSubWindow_plot(QWidget *widget);
     void loadSubWindow_setup(QWidget *widget);
-
     void loadSubWindow_fftsetup(QWidget *widget);
 
+    int osSetupWinCount = 0;
 
+    QMdiSubWindow *osSetupWin;
+    fftsetup *fftSetupObj = new fftsetup;
 
 };
 
